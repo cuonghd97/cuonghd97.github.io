25583d94666cbd60ed6f675e335c15f86efc5e19
DESKTOP-E8OTJ33\Cuong <cuong.hoangduc@cxview.ai>
2022-03-13 15:55:06 +0000

    Add pacman

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import styles from "./navbar.module.css";
-
+import pacman from "../../assets/imgs/pacman.gif"
 
 export const NavBar = () => {
     return (
-        <div>
+        <div className={styles.navbar}>
             <span className={styles.header}>Empty Blog</span>
+            <img src={pacman} alt="pacman" className={styles.pacman}/>
         </div>
     );
 };
