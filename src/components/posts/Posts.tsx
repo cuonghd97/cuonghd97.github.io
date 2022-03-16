e6cf4c0276e1a00b0ac554abb5d2162da3b4c6cb
DESKTOP-E8OTJ33\Cuong <cuong.hoangduc@cxview.ai>
2022-03-16 16:10:24 +0000

    Get list post from firestore

--- src/components/posts/Posts.tsx
+++ src/components/posts/Posts.tsx
@@ -2,33 +2,50 @@ import * as React from 'react'
 import styles from './posts.module.css'
 import { PostItem } from '../post-item/PostItem'
 import PostsDataService from '../../services/posts.service'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import { PostModel } from '../../models/post'
 
 type Props = {}
 export const Posts = (props: Props) => {
     useEffect(() => {
-        getListsPost().then(() => {
-            console.log('Get data success')
+        getListsPost().catch((error) => {
+            console.log(error)
         })
 
         return () => {}
     }, [])
 
+    const [listPost, setListPost] = useState<PostModel[]>([])
+
     const getListsPost = async () => {
         const listPosts = await PostsDataService.getListPosts()
-        console.log(listPosts)
+        let listPostsModel: PostModel[] = []
+        listPosts.forEach((doc) => {
+            listPostsModel.push({
+                content: doc.data()['contents'],
+                title: doc.data()['title'],
+            })
+        })
+
+        setListPost(listPostsModel)
     }
     return (
         <div className={styles.posts}>
-            <PostItem className={styles.post1} title={'Post 1'} />
-            <PostItem className={styles.post2} title={'Post 2'} />
-            <PostItem className={styles.post3} title={'Post 3'} />
-            <PostItem className={styles.post4} title={'Post 4'} />
-            <PostItem className={styles.post5} title={'Post 5'} />
-            <PostItem className={styles.post6} title={'Post 6'} />
-            <PostItem className={styles.post7} title={'Post 7'} />
-            <PostItem className={styles.post8} title={'Post 8'} />
-            <PostItem className={styles.post9} title={'Post 9'} />
+            {listPost.map((post, index) => {
+                return (
+                    <PostItem className="post" title={post.title} key={index} />
+                )
+            })}
+
+            {/*<PostItem className={styles.post1} title={'Post 1'} />*/}
+            {/*<PostItem className={styles.post2} title={'Post 2'} />*/}
+            {/*<PostItem className={styles.post3} title={'Post 3'} />*/}
+            {/*<PostItem className={styles.post4} title={'Post 4'} />*/}
+            {/*<PostItem className={styles.post5} title={'Post 5'} />*/}
+            {/*<PostItem className={styles.post6} title={'Post 6'} />*/}
+            {/*<PostItem className={styles.post7} title={'Post 7'} />*/}
+            {/*<PostItem className={styles.post8} title={'Post 8'} />*/}
+            {/*<PostItem className={styles.post9} title={'Post 9'} />*/}
         </div>
     )
 }
