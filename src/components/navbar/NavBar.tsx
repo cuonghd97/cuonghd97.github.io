28d7cd6d61df5f8ce8337165e0c5105bbd2464cb
CuongHD <cuong.hoangduc@cxview.ai>
2022-03-16 06:32:28 +0000

    Add menu to navbar

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,12 +1,22 @@
-import * as React from 'react'
-import styles from './navbar.module.css'
-import pacman from '../../assets/imgs/pacman.gif'
+import * as React from "react";
+import styles from "./navbar.module.css";
+import pacman from "../../assets/imgs/pacman.gif";
 
 export const NavBar = () => {
     return (
         <div className={styles.navbar}>
-            <span className={styles.header}>Empty Blog</span>
-            <img src={pacman} alt="pacman" className={styles.pacman} />
+            <div className={styles.headerContainer }>
+                <span className={styles.headerText}>Empty Blog</span>
+                <img src={pacman} alt="pacman" className={styles.pacman} />
+            </div>
+            <div>
+                <ul>
+                    <li>Home</li>
+                    <li>Java</li>
+                    <li>Deign pattern</li>
+                    <li>About me</li>
+                </ul>
+            </div>
         </div>
-    )
-}
+    );
+};
