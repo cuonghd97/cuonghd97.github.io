6e971d5fa2c602b781dfe94eb7546649b47a3081
cuong <cuong.hoangduc@cxview.ai>
2022-12-14 02:54:19 +0000

    reformat code

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { HomePage } from './pages/home-page/HomePage'
-import { PostContent } from './pages/post-content/PostContent'
-import { ImageGallery } from './pages/images-gallery/ImageGallery'
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HomePage } from './pages/home-page/HomePage';
+import { PostContent } from './pages/post-content/PostContent';
+import { ImageGallery } from './pages/images-gallery/ImageGallery';
 
 const App: React.FC = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/' element={<HomePage />} />
-                <Route path='post/:postID' element={<PostContent />} />
-                <Route path='/secret-gallery' element={<ImageGallery />} />
+                <Route path="/" element={<HomePage />} />
+                <Route path="post/:postID" element={<PostContent />} />
+                <Route path="/secret-gallery" element={<ImageGallery />} />
             </Routes>
         </BrowserRouter>
-    )
-}
+    );
+};
 
-export default App
+export default App;
